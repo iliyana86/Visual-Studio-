92094fe25f267396bb9ffa26e6e75b88f61753be
iliyana86 <133908422+iliyana86@users.noreply.github.com>
2024-01-30 23:57:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+if ((num1 > 0) && (num2 > 0) ||(num3<0))
+{
+
+    Console.WriteLine("negative");
+
+}
+else if ((0 > num1) || (0 > num2) || (num3<0))
+{
+    Console.WriteLine("positive");
+}
+else
+{
+    Console.WriteLine("zero");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//РЕШЕНА АБсолютно самостоятелно!!!
